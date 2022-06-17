aa72b3231c8ff6cbb95b474faba204d40da08f84
nabilcambiaso <nabil.cambiaso@gmail.com>
2022-06-17 22:52:29 +0100

    fetching data

---
+++ pages/users/index.js
@@ -0,0 +1,33 @@
+
+import styles from '../../styles/Users.module.css';
+export const getStaticProps = async () =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: {
+            users:data
+        }
+    }
+}
+
+
+const Users = ({users}) => {
+    console.log(users)
+  return (
+    <>
+      <div>
+        <h1>users list</h1>
+        {users.map((user)=>(
+            <div className={styles.single} style={{marginBottom:10,backgroundColor:"white",padding:10,paddingLeft:20,borderRadius:20}} key={user.id}>
+            <h3>full Name: {user.name + " " + user.username}</h3>
+            <h6>Email: {user.email}</h6>
+            <h6>Website: {user.website}</h6>
+            </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Users;
