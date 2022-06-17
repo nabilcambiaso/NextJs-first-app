793d1b7a37aa593b8fed4a2e8e4b17862f488626
nabilcambiaso <nabil.cambiaso@gmail.com>
2022-06-17 22:06:09 +0100

    Redirect from useEffect after 8 seconds

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,18 @@
 import Link from "next/link";
-
+import { useEffect } from "react";
+import {useRouter} from "next/router";
 const NotFound = () => {
+
+    const router = useRouter();
+
+    useEffect(() => {
+      setTimeout(() => {
+        //   this router go to go back -1 or forward 1
+        //   router.go(-1);
+          router.push('/');
+      }, 8000);
+    }, [])
+    
     return ( 
         <div className="not-found">
 <h1>Oooooops....</h1>
