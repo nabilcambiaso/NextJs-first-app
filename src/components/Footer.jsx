f883675f4e739672c8430dde74a9284f162e2cef
nabilcambiaso <nabil.cambiaso@gmail.com>
2022-06-17 21:12:33 +0100

    Initial commit with ready to go scaffold

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>Copyright 2022</div>
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <h1>Navbar</h1>
+            <a>Home</a>
+            <a>About</a>
+            <a>Lists</a>
+            <a>More</a>
+        </div>
+    )
+}
+
+export default Navbar
